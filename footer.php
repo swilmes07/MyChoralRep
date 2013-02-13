903a9f9e500c18c5ebffa897829bcb3964615165
Laura Purcell <mrs.laura.purcell@gmail.com>
2013-02-12 18:24:58 -0600

    Php Header Footers with template

---
+++ footer.php
@@ -0,0 +1,14 @@
+<div id="footer">
+					<div id="altnav">
+						<a href="#">About</a> -
+						<a href="#">Services</a> -
+						<a href="#">Portfolio</a> -
+						<a href="#">Contact Us</a> -
+						<a href="#">Terms of Trade</a>
+					</div>
+				Copyright&copy; MyChoralRep
+	 
+				Powered by <a href="localhost/hirdesigns">HIR Designs</a> 
+		</div>
+	</body>
+	</html>
---
+++ header.php
@@ -0,0 +1,31 @@
+<?php ?>
+<head>
+<meta http-equiv="Content-type" content="text/html; charset=UTF-8" />
+	    <title>MyChoralRep - Where Directors Come Together</title>
+	    <meta http-equiv="Content-Language" content="en-us" />
+	     
+	    <meta http-equiv="imagetoolbar" content="no" />
+	    <meta name="MSSmartTagsPreventParsing" content="true" />
+	     
+	    <meta name="description" content="Description" />
+	    <meta name="keywords" content="Keywords" />
+	     
+	    <meta name="author" content="HIR DESIGNS" />
+		<style type="text/css" media="all">@import "css/master.css";</style>
+</head>
+		<div id="page-container">
+			<div id="main-nav">
+				<ul class="main-menu">
+					<li id="about"><a href="index.php"><img src="images/NavAbout.jpg"/></a></li>
+					<li id="browse"><a href="#"><img src="images/NavBrowse2.jpg"/></a></li>
+					<li id="search"><a href="#"><img src="images/NavSearch.jpg"/></a></li>
+					<li id="register"><a href="registration.php"><img src="images/NavReg.jpg"/></a></li>
+				</ul>
+			</div>
+			
+			<div id="header">
+			</div>
+				
+		
+
+<?php?>
