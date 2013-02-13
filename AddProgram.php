128bad101f6e04a9c6c8e0090d563d684e4e6c42
Laura Purcell <mrs.laura.purcell@gmail.com>
2013-02-12 18:26:47 -0600

    Uses header footer php

--- AddProgram.php
+++ AddProgram.php
@@ -1,24 +1,8 @@
 <?php
 include('lock.php');
 ?>
-	 <meta name="author" content="HIR DESIGNS" />
-	     
-	    <style type="text/css" media="all">@import "css/master.css";</style>  <style type="text/css" media="all">@import "css/master.css";</style>
-	</head>
-	 
-	<body>
-		<div id="page-container">
-			<div id="main-nav">
-				<ul class="main-menu">
-					<li id="about"><a href="index.php"><img src="images/NavAbout.jpg"/></a></li>
-					<li id="browse"><a href="#"><img src="images/NavBrowse2.jpg"/></a></li>
-					<li id="search"><a href="#"><img src="images/NavSearch.jpg"/></a></li>
-					<li id="register"><a href="registration.php"><img src="images/NavReg.jpg"/></a></li>
-				</ul>
-			</div>
-			<div id="header">
-				
-			</div>
+
+		<?php include('header.php');?>
 			<div id="sidebar-a">
 				 <div class="padding">
 					<?php include('view.php'); ?>
@@ -60,18 +44,7 @@ include('lock.php');
 			</fieldset>
 			</form>
 
-			</div
-			<div id="footer">
-					<div id="altnav">
-						<a href="#">About</a> -
-						<a href="#">Services</a> -
-						<a href="#">Portfolio</a> -
-						<a href="#">Contact Us</a> -
-						<a href="#">Terms of Trade</a>
-					</div>
-				Copyright © MyChoralRep
-	 
-				Powered by <a href="localhost/hirdesigns">HIR Designs</a> 
-		</div>
+			</div>
+			<?php include("footer.php");?>
 	</body>
 	</html>
