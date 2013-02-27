40bfc0fbc0684e6eecf111b26d56873e7ec7e1c3
Laura Purcell <mrs.laura.purcell@gmail.com>
2013-02-27 15:31:59 -0600

    Fixed blue boxes in Internet Explorer
    This will need to be made to all the pages in the site

--- header.php
+++ header.php
@@ -11,10 +11,10 @@ include('lock.php');
 		<div id="page-container">
 			<div id="main-nav">
 				<ul class="main-menu">
-					<li id="about"><a href="index.php"><img src="images/NavAbout.jpg"/></a></li>
-					<li id="browse"><a href="#"><img src="images/NavBrowse2.jpg"/></a></li>
-					<li id="search"><a href="#"><img src="images/NavSearch.jpg"/></a></li>
-					<li id="register"><a href="registration.php"><img src="images/NavReg.jpg"/></a></li>
+					<li id="about"><a href="index.php"><img src="images/NavAbout.jpg" border="0"/></a></li>
+					<li id="browse"><a href="#"><img src="images/NavBrowse2.jpg" border="0"/></a></li>
+					<li id="search"><a href="#"><img src="images/NavSearch.jpg"border="0"/></a></li>
+					<li id="register"><a href="registration.php"><img src="images/NavReg.jpg" border="0"/></a></li>
 				</ul>
 			</div>
 			<div id="header">
